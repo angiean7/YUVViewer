cef37af3257f8ce3a0eea8260a3757d77f8c9901
angiean7 <i.angie.anqi@gmail.com>
2022-04-18 16:55:06 +0800

    0418

---
+++ YuvImageShow/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace YuvImageShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(67, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(487, 290);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(100, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "YUV420";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(225, 98);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "YUV422";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(362, 98);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(76, 19);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "YUV444";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(627, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ImageShow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ YuvImageShow/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace YuvImageShow
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog openDlg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private string pathname = string.Empty;     		
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openDlg = new OpenFileDialog();
+            openDlg.Filter = "YUV文件|*.*";
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openDlg.FileName;
+            }      
+        }
+
+        private int GetPixelValue(int pixelValue)
+        {
+            if (pixelValue >= 0 && pixelValue <= 255000)
+                pixelValue = pixelValue / 1000;
+            else
+                pixelValue = (pixelValue < 0) ? 0 : 255;
+            return pixelValue;
+        }
+
+        private unsafe byte[] ColorYuvToRgbImage(IntPtr buffer, long bufferLength)
+        {
+            IntPtr pImageData = buffer;
+            long nLength = bufferLength;
+            byte[] RgbImgData = new byte[nLength * 6 / 4];
+            byte pixel_y0 = 0;
+            byte pixel_cb = 0;
+            byte pixel_y1 = 0;
+            byte pixel_cr = 0;
+            int iYuvOff = 0;
+            int iRgbOff = 0;
+
+            byte* pbtYuvImageData = (byte*)pImageData;
+
+            for (iYuvOff = 0; iYuvOff < nLength; iYuvOff += 4)
+            {
+                pixel_y0 = *(pbtYuvImageData + iYuvOff + 0);
+                pixel_cb = *(pbtYuvImageData + iYuvOff + 1);//u
+                pixel_y1 = *(pbtYuvImageData + iYuvOff + 2);
+                pixel_cr = *(pbtYuvImageData + iYuvOff + 3);//v
+
+                int pixel_R = GetPixelValue((int)(1164 * (pixel_y1 - 16) + 1596 * (pixel_cr - 128)));
+                int pixel_G = GetPixelValue((int)(1164 * (pixel_y1 - 16) - 813 * (pixel_cr - 128) - 391 * (pixel_cb - 128)));
+                int pixel_B = GetPixelValue((int)(1164 * (pixel_y1 - 16) + 2018 * (pixel_cb - 128)));
+                RgbImgData[iRgbOff + 0] = (byte)pixel_B;
+                RgbImgData[iRgbOff + 1] = (byte)pixel_G;
+                RgbImgData[iRgbOff + 2] = (byte)pixel_R;
+
+                pixel_R = GetPixelValue((int)(1164 * (pixel_y1 - 16) + 1596 * (pixel_cr - 128)));
+                pixel_G = GetPixelValue((int)(1164 * (pixel_y1 - 16) - 813 * (pixel_cr - 128) - 391 * (pixel_cb - 128)));
+                pixel_B = GetPixelValue((int)(1164 * (pixel_y1 - 16) + 2018 * (pixel_cb - 128)));
+                RgbImgData[iRgbOff + 3] = (byte)pixel_B;
+                RgbImgData[iRgbOff + 4] = (byte)pixel_G;
+                RgbImgData[iRgbOff + 5] = (byte)pixel_R;
+
+                iRgbOff = iRgbOff + 6;
+            }
+            return RgbImgData;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openDlg.FileName != string.Empty)
+            {
+                try
+                {
+                    pathname = openDlg.FileName;
+                    FileStream file = File.Open(pathname, FileMode.Open);
+                    byte[] b_buffer = new byte[file.Length];
+                    file.Read(b_buffer, 0, (int)file.Length);
+
+                    IntPtr buffer = Marshal.AllocHGlobal((IntPtr)file.Length);
+                    buffer = System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(b_buffer, 0);
+                    
+                    Bitmap bmp = new Bitmap(176, 144, PixelFormat.Format24bppRgb);
+                    BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+
+                    byte[] newImgData = ColorYuvToRgbImage(buffer, (int)file.Length);
+                    //Bitmap bmp = new Bitmap(pathname);
+                    unsafe
+                    {
+                        byte* pNative = (byte*)bmpData.Scan0;
+                        //for (int len = 0; len < newImgData.Length; len++)
+                        for (int len = 0; len < 0x13000; len++)
+                        {
+                            pNative[len] = newImgData[len];
+                        }
+                    }
+                    bmp.UnlockBits(bmpData);
+
+
+
+                    //Image img = Image.FromHbitmap(bmp.GetHbitmap());
+                    pictureBox1.Image = bmp;
+                    pictureBox1.Show();
+                    pictureBox1.Refresh();
+
+                    //this.pictureBox1.Load(pathname);
+                    file.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+    }
+}
