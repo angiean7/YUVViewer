a3386f4e4dce948685519fde18b98e4418d8e94c
angiean7 <i.angie.anqi@gmail.com>
2022-04-25 18:34:08 +0800

    0425

--- YuvImageShow/Form1.cs
+++ YuvImageShow/Form1.cs
@@ -278,7 +278,6 @@ namespace YuvImageShow
                         bmp.UnlockBits(bmpData);
                         pictureBox1.Image = bmp;
                     }
-                    /*
                     else if (button_RGB.Checked)
                     {
                         //RGB
@@ -293,7 +292,7 @@ namespace YuvImageShow
                         }
                         bmp.UnlockBits(bmpData);
                         pictureBox1.Image = bmp;
-                    }*/
+                    }
                     else if (button_Y8.Checked)
                     {
                         //Y8
