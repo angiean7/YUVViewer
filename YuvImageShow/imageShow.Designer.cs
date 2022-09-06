c2fac0f2dc042d2639bcbd374f7af3af5d793154
angiean7 <i.angie.anqi@gmail.com>
2022-09-06 11:13:01 +0800

    all

--- YuvImageShow/imageShow.Designer.cs
+++ YuvImageShow/imageShow.Designer.cs
@@ -41,8 +41,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(2, -1);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1933, 1095);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 1080);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
